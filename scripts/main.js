d3737eff9825e632377d0dad3b428a9e7dd61ef6
dima <tynindima@gmail.com>
2020-05-12 19:09:46 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const canvas = document.querySelector('.visible');
+// const ctx = canvas.getContext('2d');
+// const lineJoin = ['round', 'bevel', 'miter'];
+// ctx.lineWidth = 10;
+//
+// for (let i = 0; i < 3; i++) {
+//   ctx.lineJoin = lineJoin[i];
+//   ctx.beginPath();
+//   ctx.moveTo(-5, 5 + i * 40);
+//   ctx.lineTo(35, 45 + i * 40);
+//   ctx.lineTo(75, 5 + i * 40);
+//   ctx.lineTo(115, 45 + i * 40);
+//   ctx.lineTo(155, 5 + i * 40);
+//   ctx.stroke();
+// }
